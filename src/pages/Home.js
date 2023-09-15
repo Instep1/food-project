1a51c16670f25e6ff905364deb10b04105556305
Maksim <lvutinwork@gmail.com>
2023-09-15 20:29:21 +0300

    Stage 5: Добавлена строка поиска и логика поиска блюд Создана логика сохранения в поисковой строке параметров запроса, для отправки ссылок с указанными параметрами другим пользователям. Использовался хук useSearchParams

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,20 +2,37 @@ import {useState, useEffect} from 'react'
 import { getAllCategories } from '../api';
 import Preloader from '../components/Preloader/Preloader';
 import CategoriesList from '../components/CategoriesList/CategoriesList';
+import Search from '../components/Search/Search';
+import { useSearchParams } from 'react-router-dom';
 
 const Home = () => {
     const [catalog, setCatalog] = useState([]);
+    const [filteredCatalog, setFilteredCatalog] = useState([]);
+    const [searchParams, setSearchParams] = useSearchParams();
+
+    const searchQuery = searchParams.get('search') || '';
+
+    const handleSearch = (str) => {
+        setSearchParams({search: str})
+        setFilteredCatalog(
+            catalog.filter(item => {
+                return item.strCategory.toLowerCase().includes(str.toLowerCase())
+            })
+        )
+    }
 
     useEffect(() => {
         getAllCategories().then(data => {
-            setCatalog(data.categories)
+            setCatalog(data.categories);
+            setFilteredCatalog( searchQuery ? data.categories.filter(item => item.strCategory.toLowerCase().includes(searchQuery)) : data.categories);
         })
-    }, [])
+    }, [searchQuery])
     return (
         <>
+        <Search cb={handleSearch}/>
         {
             !catalog.length ? <Preloader/> :
-            <CategoriesList catalog={catalog}/>
+            <CategoriesList catalog={filteredCatalog}/>
         }
        </>
     )
